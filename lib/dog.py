29a7156ead823a27b5c58e012f68271ffbef7966
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-08-29 12:21:52 +0300

    lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-
+#Add an instance method sit() to your Dog class that will print "The dog is sitting."
 class Dog:
     # Class body goes here
+    
+    def bark(self):
+        print("Woof!")
+    def sit(self):
+       print("The dog is sitting.")
+        
 
     #Instance method definition
-    pass
+    
